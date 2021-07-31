b1beb8bd86a9bd26ff696e1738cee52ae176ce38
LZRS <lzrscg@protonmail.com>
2021-07-31 16:03:31 -0700

    configure nodegit

---
+++ index.js
@@ -0,0 +1,18 @@
+var Git = require("nodegit");
+
+var getMostRecentCommit = function(repository) {
+  return repository.getBranchCommit("master");
+};
+
+var getCommitMessage = function(commit) {
+  return commit.message();
+};
+
+Git.Repository.open("nodegit")
+  .then(getMostRecentCommit)
+  .then(getCommitMessage)
+  .then(function(message) {
+    console.log(message);
+  });
+
+console.log('hello world');
